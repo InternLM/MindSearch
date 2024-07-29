31ba4c6e7dcc8498907da8e3fb1b02298d063374
lovesnowbest <lovesnowbest@gmail.com>
2024-07-29 13:04:33 +0800

    update readme

--- docs/zh_cn/cp_origin_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Copy *.md files from docs/ if it doesn't have a Chinese translation
-
-for filename in $(find ../en/ -name '*.md' -printf "%P\n");
-do
-    mkdir -p $(dirname $filename)
-    cp -n ../en/$filename ./$filename
-done
