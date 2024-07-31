883c66c7340a9196e29a1e166a39f86bb8a17af2
liujiangning30 <147385819+liujiangning30@users.noreply.github.com>
2024-07-31 12:13:34 +0800

    load model via transformers (#23)
    * load model via transformers
    
    * update demo internlm model

--- mindsearch/agent/models.py
+++ mindsearch/agent/models.py
@@ -1,9 +1,10 @@
 import os
 
-from lagent.llms import GPTAPI, INTERNLM2_META, LMDeployClient, LMDeployServer
+from lagent.llms import (GPTAPI, INTERNLM2_META, HFTransformerCasualLM,
+                         LMDeployClient, LMDeployServer)
 
 internlm_server = dict(type=LMDeployServer,
-                       path='internlm/internlm2_5-7b',
+                       path='internlm/internlm2_5-7b-chat',
                        model_name='internlm2',
                        meta_template=INTERNLM2_META,
                        top_p=0.8,
@@ -14,7 +15,7 @@ internlm_server = dict(type=LMDeployServer,
                        stop_words=['<|im_end|>'])
 
 internlm_client = dict(type=LMDeployClient,
-                       model_name='internlm2_5-7b',
+                       model_name='internlm2_5-7b-chat',
                        url='http://127.0.0.1:23333',
                        meta_template=INTERNLM2_META,
                        top_p=0.8,
@@ -23,6 +24,16 @@ internlm_client = dict(type=LMDeployClient,
                        max_new_tokens=8192,
                        repetition_penalty=1.02,
                        stop_words=['<|im_end|>'])
+
+internlm_hf = dict(type=HFTransformerCasualLM,
+                   path='internlm/internlm2_5-7b-chat',
+                   meta_template=INTERNLM2_META,
+                   top_p=0.8,
+                   top_k=None,
+                   temperature=1e-6,
+                   max_new_tokens=8192,
+                   repetition_penalty=1.02,
+                   stop_words=['<|im_end|>'])
 
 gpt4 = dict(type=GPTAPI,
             model_type='gpt-4-turbo',
