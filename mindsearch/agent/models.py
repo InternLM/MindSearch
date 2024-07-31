5cc01a4d98e68e657001c4b534530509eb922f8d
liujiangning <liujiangning@pjlab.org.cn>
2024-07-31 11:44:39 +0800

    load model via transformers

--- mindsearch/agent/models.py
+++ mindsearch/agent/models.py
@@ -1,6 +1,7 @@
 import os
 
-from lagent.llms import GPTAPI, INTERNLM2_META, LMDeployClient, LMDeployServer
+from lagent.llms import (GPTAPI, INTERNLM2_META, HFTransformerCasualLM,
+                         LMDeployClient, LMDeployServer)
 
 internlm_server = dict(type=LMDeployServer,
                        path='internlm/internlm2_5-7b',
@@ -23,6 +24,16 @@ internlm_client = dict(type=LMDeployClient,
                        max_new_tokens=8192,
                        repetition_penalty=1.02,
                        stop_words=['<|im_end|>'])
+
+internlm_hf = dict(type=HFTransformerCasualLM,
+                   path='internlm/internlm2_5-7b',
+                   meta_template=INTERNLM2_META,
+                   top_p=0.8,
+                   top_k=None,
+                   temperature=1e-6,
+                   max_new_tokens=8192,
+                   repetition_penalty=1.02,
+                   stop_words=['<|im_end|>'])
 
 gpt4 = dict(type=GPTAPI,
             model_type='gpt-4-turbo',
