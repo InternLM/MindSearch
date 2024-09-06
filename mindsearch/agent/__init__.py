30e7e1bd4fcd847272cf2b7e5342717c15d1e75e
braisedpork1964 <497494458@qq.com>
2024-09-06 15:53:52 +0800

    refactor

--- mindsearch/agent/__init__.py
+++ mindsearch/agent/__init__.py
@@ -1,60 +1,62 @@
 import os
 from datetime import datetime
 
-from lagent.actions import ActionExecutor, BingBrowser
+from lagent.actions import WebBrowser
+from lagent.agents.stream import get_plugin_prompt
+from lagent.prompts import InterpreterParser, PluginParser
+from lagent.utils import create_object
 
-import mindsearch.agent.models as llm_factory
-from mindsearch.agent.mindsearch_agent import (MindSearchAgent,
-                                               MindSearchProtocol)
-from mindsearch.agent.mindsearch_prompt import (
-    FINAL_RESPONSE_CN, FINAL_RESPONSE_EN, GRAPH_PROMPT_CN, GRAPH_PROMPT_EN,
-    fewshot_example_cn, fewshot_example_en, graph_fewshot_example_cn,
-    graph_fewshot_example_en, searcher_context_template_cn,
-    searcher_context_template_en, searcher_input_template_cn,
-    searcher_input_template_en, searcher_system_prompt_cn,
-    searcher_system_prompt_en)
+from . import models as llm_factory
+from .mindsearch_agent import MindSearchAgent
+from .mindsearch_prompt import (
+    FINAL_RESPONSE_CN,
+    FINAL_RESPONSE_EN,
+    GRAPH_PROMPT_CN,
+    GRAPH_PROMPT_EN,
+    searcher_context_template_cn,
+    searcher_context_template_en,
+    searcher_input_template_cn,
+    searcher_input_template_en,
+    searcher_system_prompt_cn,
+    searcher_system_prompt_en,
+)
 
 LLM = {}
 
 
-def init_agent(lang='cn', model_format='internlm_server',search_engine='DuckDuckGoSearch'):
+def init_agent(lang="cn", model_format="internlm_server", search_engine="BingSearch"):
     llm = LLM.get(model_format, None)
     if llm is None:
         llm_cfg = getattr(llm_factory, model_format)
         if llm_cfg is None:
             raise NotImplementedError
-        llm_cfg = llm_cfg.copy()
-        llm = llm_cfg.pop('type')(**llm_cfg)
+        llm = create_object(llm_cfg)
         LLM[model_format] = llm
 
-    interpreter_prompt = GRAPH_PROMPT_CN if lang == 'cn' else GRAPH_PROMPT_EN
-    plugin_prompt = searcher_system_prompt_cn if lang == 'cn' else searcher_system_prompt_en
-    if not model_format.lower().startswith('internlm'):
-        interpreter_prompt += graph_fewshot_example_cn if lang == 'cn' else graph_fewshot_example_en
-        plugin_prompt += fewshot_example_cn if lang == 'cn' else fewshot_example_en
-
+    date = datetime.now().strftime("The current date is %Y-%m-%d.")
+    plugins = [WebBrowser(searcher_type=search_engine, topk=6)]
     agent = MindSearchAgent(
         llm=llm,
-        protocol=MindSearchProtocol(meta_prompt=datetime.now().strftime(
-            'The current date is %Y-%m-%d.'),
-                                    interpreter_prompt=interpreter_prompt,
-                                    response_prompt=FINAL_RESPONSE_CN
-                                    if lang == 'cn' else FINAL_RESPONSE_EN),
+        template=date,
+        output_format=InterpreterParser(
+            template=GRAPH_PROMPT_CN if lang == "cn" else GRAPH_PROMPT_EN
+        ),
         searcher_cfg=dict(
             llm=llm,
-            plugin_executor=ActionExecutor(
-                BingBrowser(searcher_type=search_engine,
-                            topk=6,
-                            api_key=os.environ.get('BING_API_KEY',
-                                                   'YOUR BING API'))),
-            protocol=MindSearchProtocol(
-                meta_prompt=datetime.now().strftime(
-                    'The current date is %Y-%m-%d.'),
-                plugin_prompt=plugin_prompt,
+            plugins=plugins,
+            template=date,
+            output_format=PluginParser(
+                template=searcher_system_prompt_cn if lang == "cn" else searcher_system_prompt_en,
+                tool_info=get_plugin_prompt(plugins),
             ),
-            template=dict(input=searcher_input_template_cn
-                          if lang == 'cn' else searcher_input_template_en,
-                          context=searcher_context_template_cn
-                          if lang == 'cn' else searcher_context_template_en)),
-        max_turn=10)
+            user_input_template=searcher_input_template_cn
+            if lang == "cn"
+            else searcher_input_template_en,
+            user_context_template=searcher_context_template_cn
+            if lang == "cn"
+            else searcher_context_template_en,
+        ),
+        summary_prompt=FINAL_RESPONSE_CN if lang == "cn" else FINAL_RESPONSE_EN,
+        max_turn=10,
+    )
     return agent
--- mindsearch/app.py
+++ mindsearch/app.py
@@ -1,8 +1,6 @@
 import asyncio
 import json
 import logging
-from copy import deepcopy
-from dataclasses import asdict
 from typing import Dict, List, Union
 
 import janus
@@ -17,27 +15,23 @@ from mindsearch.agent import init_agent
 
 def parse_arguments():
     import argparse
-    parser = argparse.ArgumentParser(description='MindSearch API')
-    parser.add_argument('--lang', default='cn', type=str, help='Language')
-    parser.add_argument('--model_format',
-                        default='internlm_server',
-                        type=str,
-                        help='Model format')
-    parser.add_argument('--search_engine',
-                       default='DuckDuckGoSearch',
-                       type=str,
-                       help='Search engine')
+
+    parser = argparse.ArgumentParser(description="MindSearch API")
+    parser.add_argument("--lang", default="cn", type=str, help="Language")
+    parser.add_argument("--model_format", default="internlm_server", type=str, help="Model format")
+    parser.add_argument("--search_engine", default="BingSearch", type=str, help="Search engine")
     return parser.parse_args()
 
 
 args = parse_arguments()
-app = FastAPI(docs_url='/')
-
-app.add_middleware(CORSMiddleware,
-                   allow_origins=['*'],
-                   allow_credentials=True,
-                   allow_methods=['*'],
-                   allow_headers=['*'])
+app = FastAPI(docs_url="/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class GenerationParams(BaseModel):
@@ -45,19 +39,17 @@ class GenerationParams(BaseModel):
     agent_cfg: Dict = dict()
 
 
-@app.post('/solve')
+@app.post("/solve")
 async def run(request: GenerationParams):
-
     def convert_adjacency_to_tree(adjacency_input, root_name):
-
         def build_tree(node_name):
-            node = {'name': node_name, 'children': []}
+            node = {"name": node_name, "children": []}
             if node_name in adjacency_input:
                 for child in adjacency_input[node_name]:
-                    child_node = build_tree(child['name'])
-                    child_node['state'] = child['state']
-                    child_node['id'] = child['id']
-                    node['children'].append(child_node)
+                    child_node = build_tree(child["name"])
+                    child_node["state"] = child["state"]
+                    child_node["id"] = child["id"]
+                    node["children"].append(child_node)
             return node
 
         return build_tree(root_name)
@@ -70,11 +62,10 @@ async def run(request: GenerationParams):
             # Wrapping a sync generator as an async generator using run_in_executor
             def sync_generator_wrapper():
                 try:
-                    for response in agent.stream_chat(inputs):
-                        queue.sync_q.put(response)
+                    for response in agent(inputs):
+                        queue.sync_q.put((response, agent.get_steps()))
                 except Exception as e:
-                    logging.exception(
-                        f'Exception in sync_generator_wrapper: {e}')
+                    logging.exception(f"Exception in sync_generator_wrapper: {e}")
                 finally:
                     # Notify async_generator_wrapper that the data generation is complete.
                     queue.sync_q.put(None)
@@ -87,50 +78,59 @@ async def run(request: GenerationParams):
                     if response is None:  # Ensure that all elements are consumed
                         break
                     yield response
-                    if not isinstance(
-                            response,
-                            tuple) and response.state == AgentStatusCode.END:
+                    message, _ = response
+                    if (
+                        not isinstance(message.content, dict)
+                        and message.stream_state == AgentStatusCode.END
+                    ):
                         break
                 stop_event.set()  # Inform sync_generator_wrapper to stop
 
-            async for response in async_generator_wrapper():
-                if isinstance(response, tuple):
-                    agent_return, node_name = response
+            async for message, inner_steps in async_generator_wrapper():
+                if isinstance(message.content, dict):
+                    node_name = message.content["current_node"]
                 else:
-                    agent_return = response
                     node_name = None
-                origin_adj = deepcopy(agent_return.adjacency_list)
-                adjacency_list = convert_adjacency_to_tree(
-                    agent_return.adjacency_list, 'root')
-                assert adjacency_list[
-                    'name'] == 'root' and 'children' in adjacency_list
-                agent_return.adjacency_list = adjacency_list['children']
-                agent_return = asdict(agent_return)
-                agent_return['adj'] = origin_adj
-                response_json = json.dumps(dict(response=agent_return,
-                                                current_node=node_name),
-                                           ensure_ascii=False)
-                yield {'data': response_json}
+                node, origin_adjacency_list = (
+                    message.formatted["node"],
+                    message.formatted["adjacency_list"],
+                )
+                adjacency_list = convert_adjacency_to_tree(origin_adjacency_list, "root")
+                assert adjacency_list["name"] == "root" and "children" in adjacency_list
+                message.formatted["adj"] = adjacency_list["children"]
+                response_json = json.dumps(
+                    dict(
+                        response=message.model_dump(),
+                        current_node=node_name,
+                        node=node,
+                        adjacency_list=origin_adjacency_list,
+                        inner_steps=inner_steps,
+                    ),
+                    ensure_ascii=False,
+                )
+                yield {"data": response_json}
                 # yield f'data: {response_json}\n\n'
         except Exception as exc:
-            msg = 'An error occurred while generating the response.'
+            msg = "An error occurred while generating the response."
             logging.exception(msg)
             response_json = json.dumps(
-                dict(error=dict(msg=msg, details=str(exc))),
-                ensure_ascii=False)
-            yield {'data': response_json}
+                dict(error=dict(msg=msg, details=str(exc))), ensure_ascii=False
+            )
+            yield {"data": response_json}
             # yield f'data: {response_json}\n\n'
         finally:
-            await stop_event.wait(
-            )  # Waiting for async_generator_wrapper to stop
+            await stop_event.wait()  # Waiting for async_generator_wrapper to stop
             queue.close()
             await queue.wait_closed()
 
     inputs = request.inputs
-    agent = init_agent(lang=args.lang, model_format=args.model_format,search_engine=args.search_engine)
+    agent = init_agent(
+        lang=args.lang, model_format=args.model_format, search_engine=args.search_engine
+    )
     return EventSourceResponse(generate())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8002, log_level='info')
+
+    uvicorn.run(app, host="0.0.0.0", port=8002, log_level="info")
